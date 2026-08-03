a7e2176dc3f433cb3aa433c156d53449157d2886
vaishnavi lokare <vaishnavilokare38@gmail.com>
2026-08-03 14:17:02 +0530

    Update array.c

--- array.c
+++ array.c
@@ -4,12 +4,12 @@ int main() {
     int myNumbers[] = {25, 50, 75, 100};
 
   
-    printf("%d\n", myNumbers[0]); // Outputs the first element: 25
-    printf("%d\n", myNumbers[2]); // Outputs the third element: 75
+    printf("%d\n", myNumbers[0]);
+    printf("%d\n", myNumbers[2]); 
 
   
-    myNumbers[0] = 33;            // Changes 25 to 33
-    printf("%d\n", myNumbers[0]); // Outputs: 33
+    myNumbers[0] = 33;            
+    printf("%d\n", myNumbers[0]); 
 
     return 0;
 }
